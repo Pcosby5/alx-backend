af817365a5581094d1bc65ed529a1b9d1d89a83f
Pcosby5 <pcosby50@gmail.com>
2024-05-28 20:29:37 +0200

    2-app.py

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+This module sets up a basic Flask application with Babel for i18n support.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """
+    Configuration class for Flask application.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Determine the best match for supported languages.
+
+    Returns:
+        str: The best match language.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Route for the home page.
+
+    Returns:
+        str: The rendered template for the home page.
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
